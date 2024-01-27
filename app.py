8bda54adf82a5c08353aad41ddd98a1a22be7962
jongyoonkim <mw18074@alumni.bristol.ac.uk>
2024-01-27 23:59:13 +0900

    auth added

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 import os
 import time
+from os.path import join, dirname
 
+from dotenv import load_dotenv
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from src.inspector import Monitor
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 inspector = Monitor(update_interval=10)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+users = {
+    os.environ.get("username"): generate_password_hash(os.environ.get("password"))
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 
 @app.route("/")
+@auth.login_required
 def hello_world():
     current_time = time.time()
     if current_time - inspector.last_update > inspector.update_interval:
